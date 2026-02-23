3e0c1fd2bd73489394c80fac94bada732a198429
Bobillet <elliot.bobillet@hotmail.fr>
2026-02-23 17:05:31 +0100

    T1 + T7 - Hide password

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -12,17 +12,6 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 export class AppComponent {
 }
 
-//TODO List : 
-//- Créer un git et push dessus le front et le back
-//- Page login = Cacher le mdp quand on le saisit
-//- Back-end NodeJS = Rangé les controller et les routes
-//- Ouverture d'une pop-up sur la page Mission pour afficher le détail du ticket avec un bouton "Accepter la mission" qui ajoute la mission dans nos mission en cours. 
-//    (User les prend et elle s'ajout dans notre résumé des missions en cours.)
-//- Ajouter btn indiquant qu'une mission est finit, coté BDD un boolean true/fasle (fait/pas fait).
-//- Ajouter un compteur de mission réalisé chez l'user
-//- Toute les X missions réalisés, passage de grade pour l'user (dans son profil un résumé des mission réalisé avec son grade affiché) + ? nb de ticket a faire pour prochain grade ?
-//- Ré-écrire app-table-generic format mat-table ?
-
 /*
 Copyright Google LLC. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -2,17 +2,21 @@ import { Component } from '@angular/core';
 import { AuthService } from '../service/auth.service';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon'
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, MatIconModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   username = '';
   pwd = '';
+  
+  //Icon visibility
+  showPwd = false;
 
   constructor(private auth: AuthService, private router: Router) { }
 
